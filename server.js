43c3db047553f979aafec6e2a94ada9fdccd958b
Pete McPherson <pnm326@gmail.com>
2023-04-13 14:59:53 -0400

    changed frontend api url to localhost and allowed all cors

--- server.js
+++ server.js
@@ -22,9 +22,23 @@ app.use((req, res, next) => {
 });
 
 const cors = require('cors');
-app.use(cors({
-    origin: process.env.API_URL
-}));
+const allowedOrigins = [
+    "https://promptimizer-api.onrender.com",
+    "http://localhost:3000",
+];
+
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (process.env.NODE_ENV === "development" || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error("Not allowed by CORS"));
+//         }
+//     },
+// }));
+
+app.use(cors());
+
 
 
 
