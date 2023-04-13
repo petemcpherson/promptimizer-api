5160460aba58d0ddb44816575366058a19d987fc
Pete McPherson <pnm326@gmail.com>
2023-04-13 10:25:14 -0400

    fixing serving static files

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 const brandsRouter = require('./routes/brands');
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/posts');
-
-// const path = require('path');
-// require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
+const path = require('path');
 
 
 // const { Router } = require('express');
@@ -23,10 +21,22 @@ app.use((req, res, next) => {
     next()
 });
 
+
 // routes
 app.use('/api/brands', brandsRouter);
 app.use('/api/user', userRoutes);
 app.use('/api/posts', postRoutes);
+
+// serve static files from react frontend if in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder from the react frontend
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    // Handle any requests that don't match the ones above
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    });
+}
 
 // connect to mongodb
 const dbURI = process.env.MONGO_URI;
